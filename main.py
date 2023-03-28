53f1d50d5f9e94f1a824fe53ad27ffd6de33a926
anorth <445306+anorth@users.noreply.github.com>
2023-03-29 10:45:29 +1300

    Refactor top-level into Simulator class.

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import collections
 import decimal
 import json
 import sys
+import time
+from dataclasses import dataclass
 from decimal import Decimal
+from typing import Iterable, Dict, List
 
 from consts import EXBIBYTE, PEBIBYTE, DAY
 from miner import MinerState
@@ -11,7 +15,7 @@ def main(args):
     # TODO flags
     # epochs := flag.Int("epochs", math.MaxInt32, "epochs to simulate")
     epochs = 366 * DAY
-    print_interval = DAY
+    stats_interval = DAY
 
     # Establish decimal context for FIL tokens.
     # NOTE: A precision of 18 is not the same as 18 decimal places, it's precision of
@@ -20,39 +24,89 @@ def main(args):
     c = decimal.getcontext()
     c.prec = 18
 
-    network_power = 18 * EXBIBYTE
-    power_baseline = network_power
-    epoch_reward = Decimal("90.0")
-    circulating_supply = Decimal("439_000_000")
-    net = NetworkState(0, network_power, power_baseline, circulating_supply, epoch_reward)
+    cfg = Config(
+        network_epoch=0,
+        network_power=18 * EXBIBYTE,
+        network_epoch_reward=Decimal("90"),
+        network_circulating_supply=Decimal("439_000_000"),
 
-    balance = 10 * Decimal("1000")
-    m = MinerState(balance)
-    s = MinerStrategy(1 * PEBIBYTE, 365 * DAY)
-    rew = RewardEmitter()
+        miner_balance=Decimal("10_000"),
 
-    # Loop over epochs
-    first_epoch = net.epoch
-    for epoch in range(first_epoch, epochs):
-        net.epoch = epoch
+        strategy_initial_power=1 * PEBIBYTE,
+        strategy_initial_duration=365 * DAY,
+    )
+    sim = Simulator(cfg)
 
-        # Emit rewards according to power at start of epoch.
-        rew.emit(net, m)
+    start_time = time.perf_counter()
+    stats = sim.run_all(epochs, stats_interval)
+    end_time = time.perf_counter()
 
-        # Execute miner strategy.
-        s.act(net, m)
+    for s in stats:
+        print(json.dumps(s, cls=EnhancedJSONEncoder))
+    latency = end_time - start_time
+    print("Simulated {} epochs in {:.1f} sec".format(epochs, latency))
 
-        # Perform automatic state updates.
-        m.handle_epoch(net)
+@dataclass
+class Config:
+    network_epoch: int
+    network_power: int
+    network_epoch_reward: Decimal
+    network_circulating_supply: Decimal
 
-        if epoch % print_interval == 0:
-            stats = {
-                'day': epoch // DAY,
-                'epoch': epoch,
-            }
-            stats.update(m.summary())
-            j = json.dumps(stats, cls=EnhancedJSONEncoder)
-            print(j)
+    miner_balance: Decimal
+
+    strategy_initial_power: int
+    strategy_initial_duration: int
+
+class Simulator:
+    """A simulator for a single miner's strategy in a network context."""
+
+    def __init__(self, cfg: Config):
+        power_baseline = 0  # TODO: derive baseline from network epoch instead
+        self.net = NetworkState(cfg.network_epoch, cfg.network_power, power_baseline, cfg.network_circulating_supply,
+            cfg.network_epoch_reward)
+        self.miner = MinerState(cfg.miner_balance)
+        self.strategy = MinerStrategy(cfg.strategy_initial_power, cfg.strategy_initial_duration)
+        self.rewards = RewardEmitter()
+
+    def run(self, epochs, stats_interval=1) -> Iterable[Dict]:
+        """
+        Executes some epochs of simulation.
+        This function is a generator, yielding statistics after each `stats_interval` epochs.
+        """
+        first_epoch = self.net.epoch
+        for epoch in range(first_epoch, epochs):
+            self.net.epoch = epoch
+
+            # Emit rewards according to power at start of epoch.
+            self.rewards.emit(self.net, self.miner)
+
+            # Execute miner strategy.
+            self.strategy.act(self.net, self.miner)
+
+            # Perform automatic state updates.
+            self.miner.handle_epoch(self.net)
+
+            if epoch % stats_interval == 0:
+                yield self.stats()
+
+    def run_all(self, epochs, stats_interval=1) -> List[Dict]:
+        """
+        Executes some epochs of simulation to completion.
+        Returns the statistics collected each stats_interval epochs, and at completion.
+        """
+        stats = list(self.run(epochs, stats_interval))
+        if stats and stats[-1]['epoch'] != self.net.epoch:
+            stats.append(self.stats())
+        return stats
+
+    def stats(self) -> Dict:
+        stats = {
+            'day': self.net.epoch // DAY,
+            'epoch': self.net.epoch,
+        }
+        stats.update(self.miner.summary())
+        return stats
 
 class MinerStrategy:
     # Power to onboard immediately.
